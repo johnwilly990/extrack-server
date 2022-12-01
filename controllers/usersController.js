e0e237d515a59a6f8e584e9d1261568582db989b
johnwilly990 <j.kim1940@gmail.com>
2022-12-01 13:49:00 -0500

    Add controller for user registration

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,11 +1,17 @@
+const { v4: uuid } = require("uuid");
+const bcrypt = require("bcrypt");
+const { validationResult } = require("express-validator");
 const knex = require("knex");
 const knexConfig = require("../knexfile").development;
 const db = knex(knexConfig);
-const { v4: uuid } = require("uuid");
 
-exports.getAll = (_req, res) => {
-  db("users")
-    .select(
+const hashPassword = (password) => {
+  return bcrypt.hashSync(password, 10);
+};
+
+exports.getAll = async (_req, res) => {
+  try {
+    const response = await db("users").select(
       "id",
       "username",
       "first_name",
@@ -16,7 +22,63 @@ exports.getAll = (_req, res) => {
       "saving_amount",
       "investment_amount",
       "flexible_expense_amount"
-    )
-    .then((data) => res.status(200).json(data))
-    .catch((err) => res.status(404).json({ message: err }));
+    );
+
+    if (!response.length) {
+      return res.status(404).json({ message: "no reponse found" });
+    }
+
+    res.status(200).json(response);
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
 };
+
+// Controller for registering new user
+exports.userSignUp = async (req, res) => {
+  try {
+    const { username, password, first_name, last_name } = req.body;
+
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({
+        success: false,
+        errors: errors.array(),
+      });
+    }
+
+    // New users object
+    const newUser = {
+      id: uuid(),
+      username: username,
+      password: hashPassword(password),
+      first_name: first_name,
+      last_name: last_name,
+      income_amount: 0,
+      budget_amount: 0,
+      recurring_amount: 0,
+      saving_amount: 0,
+      investment_amount: 0,
+      flexible_expense_amount: 0,
+    };
+
+    // Retrieves username of db that matches username inputted by user
+    const usernameValidation = await db("users").where({
+      username: newUser.username,
+    });
+
+    // Validation to insert new user into db; if username already exits, do not insert to db
+    if (usernameValidation.length === 0) await db("users").insert(newUser);
+
+    // Repsonse sent back upon success
+    res.status(200).json({
+      success: true,
+      message: "Sign up successful",
+    });
+  } catch (err) {
+    res.status(500).json({ message: err });
+  }
+};
+
+//c0595d8bdd466bd6e6bf226169d51ea582e51954a26ec1b517465362e6b4f9cf
--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const router = express.Router();
+const { body } = require("express-validator");
 const usersController = require("../controllers/usersController");
 
-router.route("/").get(usersController.getAll);
+// router.route("/").get(usersController.getAll);
+
+router.route("/register").post(
+  body("username").isEmail().normalizeEmail(),
+  body("password").isLength({
+    min: 8,
+  }),
+  body("first_name").isString(),
+  body("last_name").isString(),
+  usersController.userSignUp
+);
 
 module.exports = router;
