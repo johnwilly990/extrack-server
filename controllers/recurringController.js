8ec61450e7d2c1994ac9a1c68db2f11be40d01a1
johnwilly990 <j.kim1940@gmail.com>
2022-12-06 16:29:19 -0500

    Add controller for adding new entry

--- controllers/recurringController.js
+++ controllers/recurringController.js
@@ -1,4 +1,5 @@
 const knex = require("knex");
 const knexConfig = require("../knexfile").development;
 const db = knex(knexConfig);
-const { v4: uuid } = require("uuid");
+
+exports.addEntry = async (req, res) => {};
--- routes/expenses.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const expensesController = require("../controllers/expensesController");
-
-module.exports = router;
