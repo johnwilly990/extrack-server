2160c49d28b40be5af2e195aecbdb012ac3f0278
johnwilly990 <j.kim1940@gmail.com>
2022-12-06 23:14:33 -0500

    Add controller for updating entry

--- controllers/recurringController.js
+++ controllers/recurringController.js
@@ -19,7 +19,7 @@ exports.addEntry = async (req, res) => {
 
     // Verify valid token
     const token = authorization.split(" ")[1];
-    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
+    jwt.verify(token, SECRET_KEY, async (err, _decoded) => {
       if (err) {
         return res.status(401).json({
           message: "Invalid token",
@@ -58,6 +58,66 @@ exports.addEntry = async (req, res) => {
         .update({ recurring_amount: sumOfReccuring });
 
       return res.status(201).json({ message: "New entry successfully added" });
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
+
+exports.updateEntry = async (req, res) => {
+  try {
+    const { item_name, amount, category, user_id, id } = req.body;
+    const { authorization } = req.headers;
+
+    // Validate empty fields and negative or 0 values
+    if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
+      return res.status(400).json({
+        message: "All fields required/Please enter correct numerical amount",
+      });
+    }
+
+    // Verify valid token
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Invalid token",
+        });
+      }
+
+      // New object to be added into db
+      const entry = {
+        user_id: user_id,
+        item_name: item_name,
+        amount: amount,
+        category: category,
+      };
+
+      // Inserts new entry to db
+      await db("recurring_expenses_entries")
+        .where({ user_id: user_id, item_name: item_name })
+        .update(entry);
+
+      // Sums all recurring expenses of same user ID
+      const sumRecurringArr = await db("recurring_expenses_entries")
+        .select("user_id")
+        .sum("amount")
+        .groupBy("user_id");
+
+      // Returns object with matching user Id
+      const correspondingId = sumRecurringArr.find(
+        (sum) => sum.user_id === entry.user_id
+      );
+
+      // Retrieves sum value
+      const sumOfReccuring = Object.values(correspondingId)[1];
+
+      // Updates Users table with summed value
+      await db("users")
+        .where({ id: user_id })
+        .update({ recurring_amount: sumOfReccuring });
+
+      return res.status(200).json({ message: "Entry successfully updated" });
     });
   } catch (err) {
     return res.status(500).json({ message: err });
--- routes/recurringExpenses.js
+++ routes/recurringExpenses.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const recurringController = require("../controllers/recurringController");
 
 router.post("/add", recurringController.addEntry);
+router.put("/update", recurringController.updateEntry);
 
 module.exports = router;
