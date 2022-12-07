6709befe6100526be99f81366e131bd4ecf8936a
johnwilly990 <j.kim1940@gmail.com>
2022-12-07 15:47:06 -0500

    Add controller for updating flexible expense entry

--- controllers/flexibleController.js
+++ controllers/flexibleController.js
@@ -65,7 +65,66 @@ exports.addEntry = async (req, res) => {
   }
 };
 
-exports.updateEntry = async (res, req) => {};
+// Update flexible expense entry
+exports.updateEntry = async (req, res) => {
+  try {
+    const { item_name, amount, user_id, category } = req.body;
+    const { authorization } = req.headers;
+
+    // Validate empty fields and negative or 0 values
+    if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
+      return res.status(400).json({
+        message: "All fields required/Please enter correct numerical amount",
+      });
+    }
+
+    // Verify valid token
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, SECRET_KEY, async (err, _decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Invalid token",
+        });
+      }
+
+      // New object to be added into db
+      const entry = {
+        user_id: user_id,
+        item_name: item_name,
+        amount: amount,
+        category: category,
+      };
+
+      // Inserts new entry to db
+      await db("flexible_expenses_entries")
+        .where({ user_id: user_id, item_name: item_name })
+        .update(entry);
+
+      // Sums all recurring expenses of same user ID
+      const sumRecurringArr = await db("flexible_expenses_entries")
+        .select("user_id")
+        .sum("amount")
+        .groupBy("user_id");
+
+      // Returns object with matching user Id
+      const correspondingId = sumRecurringArr.find(
+        (sum) => sum.user_id === entry.user_id
+      );
+
+      // Retrieves sum value
+      const sumOfReccuring = Object.values(correspondingId)[1];
+
+      // Updates Users table with summed value
+      await db("users")
+        .where({ id: user_id })
+        .update({ flexible_expense_amount: sumOfReccuring });
+
+      return res.status(200).json({ message: "Entry successfully updated" });
+    });
+  } catch (err) {
+    return res.status(500).json({ message: err });
+  }
+};
 
 // Get all flexible expense entries
 exports.getAllEntries = (req, res) => {
