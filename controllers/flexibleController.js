7e655e8b616a0599dff1d1bf8d0cad236b14a418
johnwilly990 <j.kim1940@gmail.com>
2022-12-07 15:40:04 -0500

    Add controller for getting all flexible expense entries

--- controllers/flexibleController.js
+++ controllers/flexibleController.js
@@ -4,3 +4,32 @@ const db = knex(knexConfig);
 const { v4: uuid } = require("uuid");
 const jwt = require("jsonwebtoken");
 const { SECRET_KEY } = process.env;
+
+exports.addEntry = async (res, req) => {};
+exports.updateEntry = async (res, req) => {};
+
+// Get all flexible expense entries
+exports.getAllEntries = (res, req) => {
+  const { authorization } = req.headers;
+
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, SECRET_KEY, async (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        message: "Invalid token",
+      });
+    }
+
+    const users = await db("flexible_expenses_entries")
+      .where({
+        user_id: decoded.id,
+      })
+      .select("id", "item_name", "amount", "category");
+
+    if (users.length === 0) {
+      return res.status(400).json({ message: "No flexible expenses found" });
+    }
+
+    return res.status(200).json(users);
+  });
+};
--- routes/flexibleExpneses.js
+++ routes/flexibleExpneses.js
@@ -2,4 +2,10 @@ const express = require("express");
 const router = express.Router();
 const flexibleController = require("../controllers/flexibleController");
 
+router
+  .route("/")
+  .post(flexibleController.addEntry)
+  .put(flexibleController.updateEntry)
+  .get(flexibleController.getAllEntries);
+
 module.exports = router;
