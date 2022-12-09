4af43f70f46b04457c78967e53b5a15d90b1b963
johnwilly990 <j.kim1940@gmail.com>
2022-12-09 10:32:24 -0500

    Complete delete controller using regular promise syntax

--- controllers/recurringController.js
+++ controllers/recurringController.js
@@ -126,53 +126,109 @@ exports.updateEntry = async (req, res) => {
 
 exports.deleteEntry = async (req, res) => {
   try {
-    // const { id } = req.params;
-    // const { authorization } = req.headers;
-    // const databaseId = await db("recurring_expenses_entries").where({ id: id });
-    // console.log(databaseId);
-    // if (!databaseId.length) {
-    //   return res.status(400).json({
-    //     message: `Invalid ID ${id}`,
-    //   });
-    // }
-    // const token = authorization.split(" ")[1];
-    // jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-    //   if (err) {
-    //     return res.status(401).json({
-    //       message: "Invalid token",
-    //     });
-    //   }
-    //   // Sums all recurring expenses of same user ID
-    //   const sumRecurringArr = await db("recurring_expenses_entries")
-    //     .select("user_id")
-    //     .sum("amount")
-    //     .groupBy("user_id");
-    //   // Deletes entry based on ID
-    //   await db("recurring_expenses_entries").del().where({ id: id });
-    //   // Returns object with matching user Id
-    //   const correspondingId = sumRecurringArr.find(
-    //     (sum) => sum.user_id === decoded.id
-    //   );
-    //   console.log(correspondingId);
-    //   // Retrieves sum value
-    //   const sumOfReccuring = Object.values(correspondingId)[1];
-    //   // Updates Users table with summed value
-    //   await db("users")
-    //     .where({ id: decoded.id })
-    //     .update({
-    //       recurring_amount: sumRecurringArr === [] ? 0 : sumOfReccuring,
-    //     });
-    //   return res.status(200).json({
-    //     message: `Entry successfully deleted. Recurring amount set to ${
-    //       sumRecurringArr === [] ? 0 : sumOfReccuring
-    //     }`,
-    //   });
-    // });
+    const { id } = req.params;
+    const { authorization } = req.headers;
+
+    // Checks if ID exists in db
+    const databaseId = await db("recurring_expenses_entries").where({ id: id });
+    if (!databaseId.length) {
+      return res.status(400).json({
+        message: `Invalid ID ${id}`,
+      });
+    }
+
+    const token = authorization.split(" ")[1];
+    jwt.verify(token, SECRET_KEY, (err, decoded) => {
+      if (err) {
+        return res.status(401).json({
+          message: "Invalid token",
+        });
+      }
+
+      // Delete entry
+      db("recurring_expenses_entries")
+        .del()
+        .where({ id: id })
+        .then(() => {
+          // Sums all entries
+          db("recurring_expenses_entries")
+            .select("user_id")
+            .sum("amount")
+            .groupBy("user_id")
+            .then((data) => {
+              // Finds the corresponding sum of user ID
+              const correspondingId = data.find(
+                (sum) => sum.user_id === decoded.id
+              );
+
+              // If no sum of specified user ID not found, assign recurring_amount in users table to 0
+              if (!correspondingId) {
+                db("users")
+                  .where({ id: decoded.id })
+                  .update({
+                    recurring_amount: 0,
+                  })
+                  .then((data) => console.log(data));
+
+                return res.status(200).json({
+                  message:
+                    "Entry successfully deleted. Recurring amount set to 0",
+                });
+              }
+
+              // Grab just numerical value of sum and assign it to recurring amount in users table
+              const sumOfReccuring = Object.values(correspondingId)[1];
+              db("users").where({ id: decoded.id }).update({
+                recurring_amount: sumOfReccuring,
+              });
+
+              return res.status(200).json({
+                message: `Entry successfully deleted. Recurring amount set to ${sumOfReccuring}`,
+              });
+            });
+        })
+        .catch((err) => {
+          return res.status(400).json({ message: err });
+        });
+      /*
+      // Deletes entry based on ID
+      await db("recurring_expenses_entries").del().where({ id: id });
+
+      // Sums all recurring expenses of same user ID
+      const sumRecurringArr = await db("recurring_expenses_entries")
+        .select("user_id")
+        .sum("amount")
+        .groupBy("user_id");
+
+      // Returns object with matching user Id
+      const correspondingId = sumRecurringArr.find(
+        (sum) => sum.user_id === decoded.id
+      );
+      console.log(correspondingId);
+
+      // Retrieves sum value
+      const sumOfReccuring = Object.values(correspondingId)[1];
+
+      // Updates Users table with summed value
+      await db("users")
+        .where({ id: decoded.id })
+        .update({
+          recurring_amount: sumOfReccuring === undefined ? sumOfReccuring : 0,
+        });
+
+      return res.status(200).json({
+        message: `Entry successfully deleted. Recurring amount set to ${
+          sumOfReccuring === undefined ? sumOfReccuring : 0
+        }`,
+      });
+      */
+    });
   } catch (err) {
     return res.status(500).json({ message: err });
   }
 };
 
+// Gets all entries of user
 exports.getAllEntries = (req, res) => {
   const { authorization } = req.headers;
 
@@ -184,12 +240,14 @@ exports.getAllEntries = (req, res) => {
       });
     }
 
+    // Gets all entries by user_id
     const users = await db("recurring_expenses_entries")
       .where({
         user_id: decoded.id,
       })
       .select("id", "item_name", "amount", "category");
 
+    // Returns error response if no entries
     if (users.length === 0) {
       return res.status(400).json({ message: "No recurring expenses found" });
     }
