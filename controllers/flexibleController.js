2e35adc3f502dfaae3f0d4921ad1573e9b32d09d
johnwilly990 <j.kim1940@gmail.com>
2022-12-20 22:02:12 -0500

    Refactor code into functions that're being called in controllers

--- controllers/flexibleController.js
+++ controllers/flexibleController.js
@@ -2,58 +2,81 @@ const knex = require("knex");
 const knexConfig = require("../knexfile").development;
 const db = knex(knexConfig);
 const { v4: uuid } = require("uuid");
-const jwt = require("jsonwebtoken");
-const { SECRET_KEY } = process.env;
+
+const sumEntry = async (userId) => {
+  // Sums all flexible expenses of same user ID
+  const sumRecurringArr = await db("flexible_entries")
+    .where("user_id", userId)
+    .sum("amount");
+
+  // Retrieves sum value
+  const sumOfReccuring = Object.values(sumRecurringArr[0])[0];
+
+  // Updates Users table with summed value
+  await db("users")
+    .where({ id: userId })
+    .update({ flexible_expense_amount: sumOfReccuring });
+};
+
+const sumBudget = async (userId) => {
+  // Sums budget
+  const budgetArr = await db("users")
+    .where({ id: userId })
+    .select(
+      "income_amount",
+      "recurring_amount",
+      "saving_amount",
+      "flexible_expense_amount",
+      "investment_amount"
+    );
+
+  // Selects first index
+  const budget = budgetArr[0];
+
+  // Calculates budget
+  const sum =
+    budget.income_amount -
+    budget.recurring_amount -
+    budget.saving_amount -
+    budget.investment_amount -
+    budget.flexible_expense_amount;
+
+  // Updates budget_amount in table
+  await db("users").where({ id: userId }).update({ budget_amount: sum });
+};
+
+const validation = (item_name, category, user_id, amount) => {
+  // Validate empty fields and negative or 0 values
+  if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
+    return res.status(400).json({
+      message: "All fields required/Please enter correct numerical amount",
+    });
+  }
+};
 
 // Add flexible expense entry
 exports.addEntry = async (req, res) => {
   try {
     const { item_name, amount, category, user_id } = req.body;
-    const { authorization } = req.headers;
 
-    // Validate empty fields and negative or 0 values
-    if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
-      return res.status(400).json({
-        message: "All fields required/Please enter correct numerical amount",
-      });
-    }
+    validation(item_name, category, user_id, amount);
 
-    // Verify valid token
-    const token = authorization.split(" ")[1];
-    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({
-          message: "Invalid token",
-        });
-      }
+    // New object to be added into db
+    const newEntry = {
+      id: uuid(),
+      user_id: req.userId,
+      item_name: item_name,
+      amount: amount,
+      category: category,
+    };
 
-      // New object to be added into db
-      const newEntry = {
-        id: uuid(),
-        user_id: decoded.id,
-        item_name: item_name,
-        amount: amount,
-        category: category,
-      };
+    // Inserts new entry to db
+    await db("flexible_entries").insert(newEntry);
 
-      // Inserts new entry to db
-      await db("flexible_entries").insert(newEntry);
+    await sumEntry(req.userId);
+    await sumBudget(req.userId);
 
-      // Sums all recurring expenses of same user ID
-      const sumRecurringArr = await db("flexible_entries")
-        .where("user_id", decoded.id)
-        .sum("amount");
-
-      // Retrieves sum value
-      const sumOfReccuring = Object.values(sumRecurringArr[0])[0];
-
-      // Updates Users table with summed value
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ flexible_expense_amount: sumOfReccuring });
-
-      return res.status(201).json({ message: "New entry successfully added" });
-    });
+    return res.status(201).json({ message: "New entry successfully added" });
   } catch (err) {
     return res.status(500).json({ message: err });
   }
@@ -64,104 +87,40 @@ exports.updateEntry = async (req, res) => {
   try {
     const { item_name, amount, user_id, category } = req.body;
     const { id } = req.params;
-    const { authorization } = req.headers;
 
-    // Validate empty fields and negative or 0 values
-    if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
-      return res.status(400).json({
-        message: "All fields required/Please enter correct numerical amount",
-      });
-    }
+    validation(item_name, category, user_id, amount);
 
-    // Verify valid token
-    const token = authorization.split(" ")[1];
-    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({
-          message: "Invalid token",
-        });
-      }
+    // New object to be added into db
+    const entry = {
+      user_id: req.userId,
+      item_name: item_name,
+      amount: amount,
+      category: category,
+    };
 
-      // New object to be added into db
-      const entry = {
-        user_id: decoded.id,
-        item_name: item_name,
-        amount: amount,
-        category: category,
-      };
+    // Inserts new entry to db
+    await db("flexible_entries").where({ id: id }).update(entry);
 
-      // Inserts new entry to db
-      await db("flexible_entries").where({ id: id }).update(entry);
+    await sumEntry(req.userId);
+    await sumBudget(req.userId);
 
-      // Sums all recurring expenses of same user ID
-      const sumRecurringArr = await db("flexible_entries")
-        .where("user_id", decoded.id)
-        .sum("amount");
-
-      // Retrieves sum value
-      const sumOfReccuring = Object.values(sumRecurringArr[0])[0];
-
-      // Updates Users table with summed value
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ flexible_expense_amount: sumOfReccuring });
-
-      // Sums budget
-      const budgetArr = await db("users")
-        .where({ id: decoded.id })
-        .select(
-          "income_amount",
-          "recurring_amount",
-          "saving_amount",
-          "flexible_expense_amount",
-          "investment_amount"
-        );
-
-      // Selects first index
-      const budget = budgetArr[0];
-
-      // Calculates budget
-      const sum =
-        budget.income_amount -
-        budget.recurring_amount -
-        budget.saving_amount -
-        budget.investment_amount -
-        budget.flexible_expense_amount;
-
-      // Updates budget_amount in table
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ budget_amount: sum });
-
-      return res.status(200).json({ message: "Entry successfully updated" });
-    });
+    return res.status(200).json({ message: "Entry successfully updated" });
   } catch (err) {
     return res.status(500).json({ message: err });
   }
 };
 
 // Get all flexible expense entries
-exports.getAllEntries = (req, res) => {
-  const { authorization } = req.headers;
+exports.getAllEntries = async (req, res) => {
+  const users = await db("flexible_entries")
+    .where({
+      user_id: req.userId,
+    })
+    .select("id", "item_name", "amount", "category");
 
-  const token = authorization.split(" ")[1];
-  jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-    if (err) {
-      return res.status(401).json({
-        message: "Invalid token",
-      });
-    }
+  if (users.length === 0) {
+    return res.status(400).json({ message: "No flexible expenses found" });
+  }
 
-    const users = await db("flexible_entries")
-      .where({
-        user_id: decoded.id,
-      })
-      .select("id", "item_name", "amount", "category");
-
-    if (users.length === 0) {
-      return res.status(400).json({ message: "No flexible expenses found" });
-    }
-
-    return res.status(200).json(users);
-  });
+  return res.status(200).json(users);
 };
--- controllers/recurringController.js
+++ controllers/recurringController.js
@@ -2,84 +2,80 @@ const knex = require("knex");
 const knexConfig = require("../knexfile").development;
 const db = knex(knexConfig);
 const { v4: uuid } = require("uuid");
-const jwt = require("jsonwebtoken");
-const { SECRET_KEY } = process.env;
+
+const sumEntry = async (userId) => {
+  // Sums all recurring expenses of same user ID
+  const sumRecurringArr = await db("recurring_entries")
+    .where("user_id", userId)
+    .sum("amount");
+
+  // Retrieves sum value
+  const sumOfReccuring = Object.values(sumRecurringArr[0])[0];
+
+  // Updates Users table with summed value
+  await db("users")
+    .where({ id: userId })
+    .update({ recurring_amount: sumOfReccuring });
+};
+
+const sumBudget = async (userId) => {
+  // Sums budget
+  const budgetArr = await db("users")
+    .where({ id: userId })
+    .select(
+      "income_amount",
+      "recurring_amount",
+      "saving_amount",
+      "flexible_expense_amount",
+      "investment_amount"
+    );
+
+  // Selects first index
+  const budget = budgetArr[0];
+
+  // Calculates budget
+  const sum =
+    budget.income_amount -
+    budget.recurring_amount -
+    budget.saving_amount -
+    budget.investment_amount -
+    budget.flexible_expense_amount;
+
+  // Updates budget_amount in table
+  await db("users").where({ id: userId }).update({ budget_amount: sum });
+};
+
+const validation = (item_name, category, user_id, amount) => {
+  // Validate empty fields and negative or 0 values
+  if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
+    return res.status(400).json({
+      message: "All fields required/Please enter correct numerical amount",
+    });
+  }
+};
 
 exports.addEntry = async (req, res) => {
   try {
     const { item_name, amount, category, user_id } = req.body;
-    const { authorization } = req.headers;
 
-    // Validate empty fields and negative or 0 values
-    if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
-      return res.status(400).json({
-        message: "All fields required/Please enter correct numerical amount",
-      });
-    }
+    validation(item_name, category, user_id, amount);
 
-    // Verify valid token
-    const token = authorization.split(" ")[1];
-    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({
-          message: "Invalid token",
-        });
-      }
+    // New object to be added into db
+    const newEntry = {
+      id: uuid(),
+      user_id: req.userId,
+      item_name: item_name,
+      amount: amount,
+      category: category,
+    };
 
-      // New object to be added into db
-      const newEntry = {
-        id: uuid(),
-        user_id: decoded.id,
-        item_name: item_name,
-        amount: amount,
-        category: category,
-      };
+    // Inserts new entry to db
+    await db("recurring_entries").insert(newEntry);
 
-      // Inserts new entry to db
-      await db("recurring_entries").insert(newEntry);
+    await sumEntry(req.userId);
+    await sumBudget(req.userId);
 
-      // Sums all recurring expenses of same user ID
-      const sumRecurringArr = await db("recurring_entries")
-        .where("user_id", decoded.id)
-        .sum("amount");
-
-      // Retrieves sum value
-      const sumOfReccuring = Object.values(sumRecurringArr[0])[0];
-
-      // Updates Users table with summed value
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ recurring_amount: sumOfReccuring });
-
-      // Sums budget
-      const budgetArr = await db("users")
-        .where({ id: decoded.id })
-        .select(
-          "income_amount",
-          "recurring_amount",
-          "saving_amount",
-          "flexible_expense_amount",
-          "investment_amount"
-        );
-
-      // Selects first index
-      const budget = budgetArr[0];
-
-      // Calculates budget
-      const sum =
-        budget.income_amount -
-        budget.recurring_amount -
-        budget.saving_amount -
-        budget.investment_amount -
-        budget.flexible_expense_amount;
-
-      // Updates budget_amount in table
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ budget_amount: sum });
-
-      return res.status(201).json({ message: "New entry successfully added" });
-    });
+    return res.status(201).send({ message: "New entry successfully added" });
   } catch (err) {
     return res.status(500).json({ message: err });
   }
@@ -89,77 +85,24 @@ exports.updateEntry = async (req, res) => {
   try {
     const { item_name, amount, user_id, category } = req.body;
     const { id } = req.params;
-    const { authorization } = req.headers;
 
-    // Validate empty fields and negative or 0 values
-    if (item_name === "" || category === "" || user_id === "" || amount <= 0) {
-      return res.status(400).json({
-        message: "All fields required/Please enter correct numerical amount",
-      });
-    }
+    validation(item_name, category, user_id, amount);
 
-    // Verify valid token
-    const token = authorization.split(" ")[1];
-    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({
-          message: "Invalid token",
-        });
-      }
+    // New object to be added into db
+    const entry = {
+      user_id: req.userId,
+      item_name: item_name,
+      amount: amount,
+      category: category,
+    };
 
-      // New object to be added into db
-      const entry = {
-        user_id: decoded.id,
-        item_name: item_name,
-        amount: amount,
-        category: category,
-      };
+    // Inserts new entry to db
+    await db("recurring_entries").where({ id: id }).update(entry);
 
-      // Inserts new entry to db
-      await db("recurring_entries").where({ id: id }).update(entry);
+    await sumEntry(req.userId);
+    await sumBudget(req.userId);
 
-      // Sums all recurring expenses of same user ID
-      const sumRecurringArr = await db("recurring_entries")
-        .where("user_id", decoded.id)
-        .sum("amount");
-
-      // Retrieves sum value
-      const sumOfReccuring = Object.values(sumRecurringArr[0])[0];
-
-      // Updates Users table with summed value
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ recurring_amount: sumOfReccuring });
-
-      // Sums budget
-      const budgetArr = await db("users")
-        .where({ id: decoded.id })
-        .select(
-          "income_amount",
-          "recurring_amount",
-          "saving_amount",
-          "flexible_expense_amount",
-          "investment_amount"
-        );
-
-      // Selects first index
-      const budget = budgetArr[0];
-
-      // Calculates budget
-      const sum =
-        budget.income_amount -
-        budget.recurring_amount -
-        budget.saving_amount -
-        budget.investment_amount -
-        budget.flexible_expense_amount;
-
-      // Updates budget_amount in table
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ budget_amount: sum });
-
-      return res.status(200).json({ message: "Entry successfully updated" });
-    });
+    return res.status(200).json({ message: "Entry successfully updated" });
   } catch (err) {
     return res.status(500).json({ message: err });
   }
@@ -168,7 +111,6 @@ exports.updateEntry = async (req, res) => {
 exports.deleteEntry = async (req, res) => {
   try {
     const { id } = req.params;
-    const { authorization } = req.headers;
 
     // Checks if ID exists in db
     const databaseId = await db("recurring_entries").where({ id: id });
@@ -178,62 +120,14 @@ exports.deleteEntry = async (req, res) => {
       });
     }
 
-    const token = authorization.split(" ")[1];
-    jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-      if (err) {
-        return res.status(401).json({
-          message: "Invalid token",
-        });
-      }
+    //Deletes entry based on ID
+    await db("recurring_entries").del().where({ id: id });
 
-      //Deletes entry based on ID
-      await db("recurring_entries").del().where({ id: id });
+    await sumEntry(req.userId);
+    await sumBudget(req.userId);
 
-      // Sums all recurring expenses of same user ID
-      const sumRecurringArr = await db("recurring_entries")
-        .where("user_id", decoded.id)
-        .sum("amount");
-
-      // Retrieves sum value
-      const sumOfReccuring = Object.values(sumRecurringArr[0])[0];
-
-      // Updates Users table with summed value
-      await db("users")
-        .where({ id: decoded.id })
-        .update({
-          recurring_amount: sumOfReccuring === null ? 0 : sumOfReccuring,
-        });
-
-      // Sums budget
-      const budgetArr = await db("users")
-        .where({ id: decoded.id })
-        .select(
-          "income_amount",
-          "recurring_amount",
-          "saving_amount",
-          "flexible_expense_amount",
-          "investment_amount"
-        );
-
-      // Selects first index
-      const budget = budgetArr[0];
-
-      // Calculates budget
-      const sum =
-        budget.income_amount -
-        budget.recurring_amount -
-        budget.saving_amount -
-        budget.investment_amount -
-        budget.flexible_expense_amount;
-
-      // Updates budget_amount in table
-      await db("users")
-        .where({ id: decoded.id })
-        .update({ budget_amount: sum });
-
-      return res.status(200).json({
-        message: "Entry successfully deleted",
-      });
+    return res.status(200).json({
+      message: "Entry successfully deleted",
     });
   } catch (err) {
     return res.status(500).json({ message: err });
@@ -241,29 +135,18 @@ exports.deleteEntry = async (req, res) => {
 };
 
 // Gets all entries of user
-exports.getAllEntries = (req, res) => {
-  const { authorization } = req.headers;
+exports.getAllEntries = async (req, res) => {
+  // Gets all entries by user_id
+  const users = await db("recurring_entries")
+    .where({
+      user_id: req.userId,
+    })
+    .select("id", "item_name", "amount", "category");
 
-  const token = authorization.split(" ")[1];
-  jwt.verify(token, SECRET_KEY, async (err, decoded) => {
-    if (err) {
-      return res.status(401).json({
-        message: "Invalid token",
-      });
-    }
+  // Returns error response if no entries
+  if (users.length === 0) {
+    return res.status(400).json({ message: "No recurring expenses found" });
+  }
 
-    // Gets all entries by user_id
-    const users = await db("recurring_entries")
-      .where({
-        user_id: decoded.id,
-      })
-      .select("id", "item_name", "amount", "category");
-
-    // Returns error response if no entries
-    if (users.length === 0) {
-      return res.status(400).json({ message: "No recurring expenses found" });
-    }
-
-    return res.status(200).json(users);
-  });
+  return res.status(200).json(users);
 };
